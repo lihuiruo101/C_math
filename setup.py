bf81e21c88e5e89482c4ad8755fe7ab6cc9659be
lihuiruo101 <yjy140206@gmail.com>
2025-05-29 16:00:04 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='C_math',
+    version='0.1',
+    description='간단한 수학 연산을 위한 모듈',
+    author='lihuiruo101',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+    ],
+)
